4889353a43dbb03e4ca4209ec28de393c67e6e61
Miguel <49310481+MiguelPerezPalma@users.noreply.github.com>
2022-02-10 08:59:27 +0100

    Model User y EchangeGift

--- src/app/models/ExchangeGift.ts
+++ src/app/models/ExchangeGift.ts
@@ -1,2 +1,10 @@
+import {Agency} from "./Agency"
+import { Gift } from "./Gift";
 export interface ExchangeGift{
+    id?:number,
+    dateEchange: Date,
+    observations: string,
+    isDelivered: boolean,
+    agency:Agency,
+    gift:Gift
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,2 +1,8 @@
 export interface User{
+    id?:number,
+    code:number,
+    password:string,
+    administrator:boolean,
+    email:string,
+    name:string
 }
