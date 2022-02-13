8bda22a194d09306c6a03313279b8c33cd070268
Miguel <49310481+MiguelPerezPalma@users.noreply.github.com>
2022-02-14 00:30:54 +0100

    cambios

--- src/app/services/exchange-gift.service.ts
+++ src/app/services/exchange-gift.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Agency } from '../models/Agency';
+import { ExchangeGift } from '../models/ExchangeGift';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,17 @@ import { Injectable } from '@angular/core';
 export class ExchangeGiftService {
 
   constructor() { }
+
+  public getAllPaged(all?):Observable<ExchangeGift[]>{
+    return null;
+  }
+  public getAll():Observable<ExchangeGift[]>{
+    return null;
+  }
+  public getByDeliveredPaged(delivered:Boolean):Observable<ExchangeGift[]>{
+    return null;
+  }
+  public getByAgebcyPaged(agency:Agency):Observable<ExchangeGift[]>{
+    return null;
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { promise } from 'protractor';
+import { Observable } from 'rxjs';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,32 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+  public getAllPaged(all?):Observable<User[]>{
+    return null;
+  }
+
+  public getAll():Observable<User[]>{
+    return null;
+  }
+  public getByCode(code:number):Promise<User>{
+    return null;
+  }
+  public getByName(name:string):Promise<User>{
+    return null;
+  }
+  public getAllUserAgenciesPaged(administrator:boolean):Observable<User[]>{
+    return null;
+  }
+
+  public getAllAdminPaged(administrator:boolean):Observable<User[]>{
+    return null;
+  }
+
+  public createOrUpdate(user:User):Promise<User>{
+    return null;
+  }
+  public delete(user:User):Promise<void>{
+    return null;
+  }
 }
