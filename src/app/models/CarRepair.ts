dd288111f1452752657a29e2cf219df85b742dd1
Jkinjt <l02jitoj@gmail.com>
2022-02-18 09:51:28 +0100

    primer commit

--- src/app/models/CarRepair.ts
+++ src/app/models/CarRepair.ts
@@ -1,2 +1,18 @@
+import {Agency} from "./Agency"
+
 export interface CarRepair{
+    id?:number,
+    operation:number,
+    carPlate:string,
+    model:string,
+    brandCar:string,
+    clientName:string,
+    dateOrder:Date,
+    nor:number,
+    amount:number,
+    dateRepair:Date,
+    asigPoints:number,
+    repaired:boolean,
+    myAgency:Agency
+
 }
