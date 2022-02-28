1f11d272cd4f6a4da0df1ff768607ffe39c758af
Jkinjt <l02jitoj@gmail.com>
2022-02-28 20:44:40 +0100

    servicios de carReapir terminado

--- src/app/administrator/carRepair/car-repair-list/car-repair-list.page.ts
+++ src/app/administrator/carRepair/car-repair-list/car-repair-list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-car-repair-list',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarRepairListPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+
+  
+
+  constructor() {
+
+   }
 
   ngOnInit() {
   }
--- src/app/services/car-repair.service.ts
+++ src/app/services/car-repair.service.ts
@@ -1,9 +1,278 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CarRepair } from '../models/CarRepair';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarRepairService {
 
-  constructor() { }
+  endpoint:string="/carRepairs";
+
+  URLDatabase:string;
+
+  constructor(
+    public http:HttpClient
+  ) {
+    this.URLDatabase=environment.herokuConfig.url;
+   }
+
+   /**
+    * Método que carga una lista de reparaciones de coches en función de la petición
+    * @param endpoint 
+    * @returns 
+    */
+   private async getListData(endpoint:string):Promise<CarRepair[]>{
+     let carRepairs:CarRepair[]=[];
+
+    return new Promise(resolve=>{
+
+      this.http.get(endpoint).subscribe((data:any[])=>{//se trae la petición 
+
+        if(data !=null && data.length>0){//comprueba que la petición se ha traido correctamente
+
+          for(let carRepair of data){
+
+            const tmp:CarRepair={//se instancia una reparacion
+
+              id:carRepair.id,
+              operation:carRepair.operation,
+              carPlate:carRepair.carPlate,
+              model:carRepair.model,
+              brandCar:carRepair.brandCar,
+              clientName:carRepair.clientName,
+              dateOrder:carRepair.Date,
+              nor:carRepair.nor,
+              amount:carRepair.amount,
+              dateRepair:carRepair.dateRepair,
+              asigPoints:carRepair.asigPoints,
+              repaired:carRepair.repaired,
+              myAgency:carRepair.myAgency
+            }
+
+            carRepairs.push(carRepair);//se añade a la lista
+            
+          }
+        }
+        resolve(carRepairs);
+      },error=>{
+        console.log(error);
+        //mandar una alerta
+      });
+    });
+   }
+
+   /**
+    * Metodo que devuelve una promesa de todas las reparaciones de la base de datos
+    * @returns Promise<CarRepair[]> con todas las reparaciones de la base de datos
+    */
+   public async getAll(): Promise<CarRepair[]>{
+
+     return this.getListData(this.URLDatabase+this.endpoint);
+
+   }
+
+   /**
+    * Método que devuevle las reparaciones de la base de datos de forma paginada.
+    * Devuelve una lista con el número de reparaciones especificadas 
+    * a partir del último elemento que se le indique 
+    * @param limit número de reparaciones que se quiera obtener
+    * @param offset a partir de que reparación se empieza a contar, con ese incliudo
+    * @returns Promise<CarRepair[]>
+    */
+   public async getAllPaged(limit:number,offset:number):Promise<CarRepair[]>{
+
+     return this.getListData(this.URLDatabase+this.endpoint+"/element/" + limit + "/page/" + offset);
+
+   }
+
+   /**
+    * Método que devuelve una reparación con la id que se le ha indicado
+    * @param id id de la reparación que se quiera obtener
+    * @returns Promise<CarRepair> con la id introducida
+    */
+   public async getById(id:number):Promise<CarRepair>{
+
+    let carRepair:CarRepair=null;
+
+    return new Promise(resolve=>{
+      this.http.get(this.URLDatabase+this.endpoint+ "/id/" + id).subscribe((a:any)=>{
+
+        if(a.id){//comprueba que la reparación que se ha traido tiene id
+          const tmp:CarRepair={
+            id:a.id,
+            operation:a.operation,
+            carPlate:a.carPlate,
+            model:a.model,
+            brandCar:a.brandCar,
+            clientName:a.clientName,
+            dateOrder:a.dateOrder,
+            nor:a.nor,
+            amount:a.amount,
+            dateRepair:a.dateRepair,
+            asigPoints:a.asigPoints,
+            repaired:a.repaired,
+            myAgency:a.myAgency
+          }
+
+          carRepair=tmp;
+        }
+
+        resolve(carRepair);
+
+      },error=>{
+        console.log(error);
+        //introducir una alerta
+        resolve(carRepair);
+      });
+
+    });
+   }
+
+   //ByOperationpaged
+   /**
+    * Método que devuelve una lista de reparaciones en función de la operación
+    * @param operation Número de la operación de la reparación
+    * @param element Número de reparaciones que se quiera obtener
+    * @param offset a partir de que reparación se va a devolver
+    * @returns Promise<CarRepair[]>
+    */
+
+   public async getByOperationPaged(operation:number,element:number,offset:number):Promise<CarRepair[]>{
+     
+    return this.getListData(this.URLDatabase+this.endpoint+"/operation/"+operation+"/elements/"+element+"/page/"+offset);
+   }
+
+   //ByCarPLate
+
+   /**
+    * Método que devuelve una lista de reparaciones en función de la matrícula
+    * @param carPlate Matrícula del coche que se quiere buscar
+    * @param element Número de reparaciones que se quiera obtener
+    * @param offset A partir de que reparación se va a devolver
+    * @returns Promise<CarRepair[]>
+    */
+   public async gerByCarPlate(carPlate:string,element:number,offset:number):Promise<CarRepair[]>{
+     
+    return this.getListData(this.URLDatabase+this.endpoint+"/carPlate/"+carPlate+"/elements/"+element+"/page/"+offset);
+   }
+
+   //ByClientName
+
+   /**
+    * Método que devuelve una lista de reparaciones en función del dueño del coche
+    * @param clientName Nombre del cliente del que se quiera obtener las reparaciones
+    * @param element Número de reparaciones que se quiera obtener
+    * @param offset A oartir de que reparación se va a devolver
+    * @returns Promise<CarRepair[]>
+    */
+   public async getByClientName(clientName:string,element:number,offset:number):Promise<CarRepair[]>{
+
+    return this.getListData(this.URLDatabase+this.endpoint+"/clientName/"+clientName+"/elements/"+element+"/page/"+offset);
+   }
+
+   //ByDateOrderPaged
+
+   /**
+    * Método que devuelve una lista de reparaciones que se encuentre entre dos fechas
+    * @param ini Fecha mas antigua
+    * @param end Fecha mas moderna
+    * @param element Número de reparaciones que se quiera obtener
+    * @param offset A partir de que elemento se quiere empezar a contar
+    * @returns 
+    */
+   public async getByDateOrderPaged(ini:Date,end:Date,element:number,offset:number):Promise<CarRepair[]>{
+
+    return this.getListData(this.URLDatabase+this.endpoint+"/ini/"+ini+"/end/"+end+"/elements/"+element+"/page/"+offset);
+   }
+
+   //ByPointsPaged
+
+   /**
+    * Método que devuelve una lista de reparaciones que tengan los puntos entre un rango determinado
+    * @param min número mínimo de puntos que se quiera buscar
+    * @param max número máximo de puntos que se quiera buscar
+    * @param element Número de reparaciones que se quiera obtener
+    * @param offset A partir de que elemento se quiere empezar a contar
+    * @returns Promise<CarRepair[]>
+    */
+   public async getByPointsPaged(min:number,max:number,element:number,offset:number):Promise<CarRepair[]>{
+
+    return this.getListData(this.URLDatabase+this.endpoint+"/min/"+min+"/max/"+max+"/elements/"+element+"/page/"+offset);
+   }
+
+   //ByStatedPaged
+
+   /**
+    * Método que devuelve una lista con las reparaciones en función de si están o no terminadas
+    * @param repaired booleano que indica si se quiere buscar las reparaciones terminadas o no
+    * @param element Número de reparaciones que se quiera obtener
+    * @param offset A partir de que elemento se quiere empezar a contar
+    * @returns Promise<CarRepair[]>
+    */
+   public async getByStatedPaged(repaired:boolean,element:number,offset:number):Promise<CarRepair[]>{
+     
+    return this.getListData(this.URLDatabase+this.endpoint+"/repaired/"+repaired+"/elements/"+element+"/page/"+offset);
+   }
+
+   //CreateOrUpdate
+
+   /**
+    * Método que guarda o actualiza una reparacion en la base de datos
+    * @param carRepair Reparación que se quiera guardar o actualizar
+    * @returns Promise<CarRepair>
+    */
+   public async createOrUpdate(carRepair:CarRepair):Promise<CarRepair>{
+     if(carRepair!=null){
+       if(carRepair.operation!=null
+        &&carRepair.carPlate!=null
+        &&carRepair.model!=null
+        &&carRepair.brandCar!=null
+        &&carRepair.clientName!=null
+        &&carRepair.dateOrder!=null
+        &&carRepair.nor!=null
+        &&carRepair.amount!=null
+        &&carRepair.dateRepair!=null
+        &&carRepair.asigPoints!=null
+        &&carRepair.repaired!=null
+        &&carRepair.myAgency!=null){
+          const body=carRepair;
+
+          return new Promise(resolve=>{
+            this.http.post(this.URLDatabase+this.endpoint,body).subscribe((data:any)=>{
+
+              carRepair=data;
+              resolve(carRepair);
+            },error=>{
+              resolve(carRepair);
+              //meter una alerta que indique que se ha producido un error
+            });
+
+          });
+
+        }else{
+          //meter una alerta que señale que algún campo es nulo
+        }
+
+     }else{
+       //meter una alerta que señale que la reparación es nula
+     }
+   }
+
+   //delete
+
+   public delete(carRepair:CarRepair):Promise<boolean>{
+
+    return new Promise(resolve=>{
+
+      this.http.delete<CarRepair>(this.URLDatabase+this.endpoint,{body:carRepair}).subscribe(()=>{
+        resolve(true);
+      },error=>{
+        resolve(false);
+        //meter una alerta que señale que no se ha borrado la reparación
+      });
+    });
+   }
+
 }
