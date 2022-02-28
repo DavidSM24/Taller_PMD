08b015b6af6c76f2d994e7884e9545a93ce68ad0
DavidSM24 <david.dam.4.1.96@gmail.com>
2022-02-28 14:03:37 +0100

    subiendo cambios temporales

--- src/app/administrator/agency/agency-list/agency-list.page.ts
+++ src/app/administrator/agency/agency-list/agency-list.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController, IonInfiniteScroll, LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { Agency } from 'src/app/models/Agency';
+import { AgencyService } from '../../../services/agency.service';
+import { AgencyUpdatePage } from '../agency-update/agency-update.page';
 
 @Component({
   selector: 'app-agency-list',
@@ -7,9 +11,152 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgencyListPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+
+  private n:number=0;
+  public agencies: Agency[] = [];
+
+  searchStr = "";
+  private miLoading: HTMLIonLoadingElement;
+
+  constructor(
+    private as: AgencyService,
+    private toast: ToastController,
+    private AlertCtrl: AlertController,
+    private modalCtrl: ModalController,
+    private loading: LoadingController) { }
 
   ngOnInit() {
   }
 
+  async ionViewWillEnter() {
+    await this.loadAgencies();
+    this.n=this.agencies.length;
+  }
+
+  public async loadAgencies(event?) {
+    
+    let newAgencies:Agency[]=[];
+    
+    if(this.agencies.length==0){ //inicio
+      
+      if(!event){ //si event, el reset ya tiene loading...
+        this.presentLoading();
+      }
+      
+      this.infinite.disabled=false;
+      newAgencies=await this.as.getAllPaged(15,0);
+      
+      this.agencies=this.agencies.concat(newAgencies);
+
+    }
+
+    if(newAgencies.length<15){
+      this.infinite.disabled=true;
+    }
+
+    if(event){
+      event.target.complete();
+    }
+    else{
+      this.loading.dismiss();
+    }
+  }
+
+  public async edit(agency:Agency){
+    const modal = await this.modalCtrl.create({
+      component: AgencyUpdatePage,
+      componentProps: {
+        agency:agency
+      }
+    });
+
+    await modal.present();
+
+    const resp=await modal.onDidDismiss();
+
+    if(resp.data!=null){
+      let i:number=this.agencies.indexOf(agency);
+      this.agencies[i]=resp.data.newAgency;
+    }
+  }
+
+  public async delete(agency:Agency){
+    await this.presentLoading();
+    const result:boolean=await this.as.delete(agency);
+    let i=this.agencies.indexOf(agency,0);
+    
+    await this.miLoading.dismiss();
+    if(result){
+      if(i>-1){
+        this.agencies.splice(i,1);
+      }
+      this.presentToast("Agencia eliminada correctamente.","success");
+    }
+    else{
+      this.presentToast("Error al eliminar la agencia...","danger");
+    }
+  }
+
+  public async showDeleteMenu(agency: Agency) {
+    const alert = await this.AlertCtrl.create({
+      header: 'Confirmación',
+      message: '¿Desea eliminar esta Agencia?',
+      buttons: [
+        {
+          text: 'Eliminar',
+          //cssClass: 'rojo',
+          handler: () => {
+            this.delete(agency);
+          }
+        },
+
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  public async infiniteLoad($event) {
+    let newAgencies:Agency[]=[];
+    if(!this.infinite.disabled){
+      newAgencies=await this.as.getAllPaged(15,this.agencies.length);
+      this.agencies=this.agencies.concat(newAgencies);
+
+      if(newAgencies.length<15){
+        this.infinite.disabled=true;
+      }
+    }
+  }
+
+  async presentLoading() {
+    this.miLoading = await this.loading.create({
+      message: ''
+    });
+    await this.miLoading.present();
+  }
+
+  async presentToast(msg: string, clr: string) {
+    const miToast = await this.toast.create({
+      message: msg,
+      duration: 2000,
+      color: clr
+    });
+    miToast.present();
+  }
+
+  public onSearchChange(event) {
+    this.searchStr = event.detail.value;
+  }
+
+  public async reset(event){
+    this.n=0;
+    this.infinite.disabled=false;
+    this.agencies=[];
+    this.loadAgencies(event);
+  }
 }
--- src/app/administrator/gift/gif-list/gif-list.page.ts
+++ src/app/administrator/gift/gif-list/gif-list.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController, IonInfiniteScroll, LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { Gift } from 'src/app/models/Gift';
+import { GiftService } from '../../../services/gift.service';
+import { GifUpdatePage } from '../gif-update/gif-update.page';
 
 @Component({
   selector: 'app-gif-list',
@@ -7,9 +11,153 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GifListPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+
+  private n:number=0;
+  public gifts: Gift[] = [];
+
+  searchStr = "";
+  private miLoading: HTMLIonLoadingElement;
+
+  constructor(
+    private gs: GiftService,
+    private toast: ToastController,
+    private AlertCtrl: AlertController,
+    private modalCtrl: ModalController,
+    private loading: LoadingController) { }
 
   ngOnInit() {
+  }
+
+  async ionViewWillEnter() {
+    await this.loadgifts();
+    this.n=this.gifts.length;
+  }
+
+  public async loadgifts(event?) {
+    
+    let newgifts:Gift[]=[];
+    
+    if(this.gifts.length==0){ //inicio
+      
+      if(!event){ //si event, el reset ya tiene loading...
+        this.presentLoading();
+      }
+      
+      this.infinite.disabled=false;
+      newgifts=await this.gs.getAllPaged(15,0);
+      
+      this.gifts=this.gifts.concat(newgifts);
+
+    }
+
+    if(newgifts.length<15){
+      this.infinite.disabled=true;
+    }
+
+    if(event){
+      event.target.complete();
+    }
+    else{
+      this.loading.dismiss();
+    }
+  }
+
+  public async edit(gift:Gift){
+    const modal = await this.modalCtrl.create({
+      component: GifUpdatePage,
+      componentProps: {
+        gift:gift
+      }
+    });
+
+    await modal.present();
+
+    const resp=await modal.onDidDismiss();
+
+    if(resp.data!=null){
+      let i:number=this.gifts.indexOf(gift);
+      this.gifts[i]=resp.data.newGift;
+    }
+  }
+
+  public async delete(gift:Gift){
+    await this.presentLoading();
+    const result:boolean=await this.gs.delete(gift);
+    let i=this.gifts.indexOf(gift,0);
+    
+    await this.miLoading.dismiss();
+    if(result){
+      if(i>-1){
+        this.gifts.splice(i,1);
+      }
+      this.presentToast("Regalo eliminada correctamente.","success");
+    }
+    else{
+      this.presentToast("Error al eliminar el regalo...","danger");
+    }
+  }
+
+  public async showDeleteMenu(gift: Gift) {
+    const alert = await this.AlertCtrl.create({
+      header: 'Confirmación',
+      message: '¿Desea eliminar este Regalo?',
+      buttons: [
+        {
+          text: 'Eliminar',
+          //cssClass: 'rojo',
+          handler: () => {
+            this.delete(gift);
+          }
+        },
+
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  public async infiniteLoad($event) {
+    let newgifts:Gift[]=[];
+    if(!this.infinite.disabled){
+      newgifts=await this.gs.getAllPaged(15,this.gifts.length);
+      this.gifts=this.gifts.concat(newgifts);
+
+      if(newgifts.length<15){
+        this.infinite.disabled=true;
+      }
+    }
+  }
+
+  async presentLoading() {
+    this.miLoading = await this.loading.create({
+      message: ''
+    });
+    await this.miLoading.present();
+  }
+
+  async presentToast(msg: string, clr: string) {
+    const miToast = await this.toast.create({
+      message: msg,
+      duration: 2000,
+      color: clr
+    });
+    miToast.present();
+  }
+
+  public onSearchChange(event) {
+    this.searchStr = event.detail.value;
+  }
+
+  public async reset(event){
+    this.n=0;
+    this.infinite.disabled=false;
+    this.gifts=[];
+    this.loadgifts(event);
   }
 
 }
