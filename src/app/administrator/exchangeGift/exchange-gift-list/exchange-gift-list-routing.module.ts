3f555c444d68ec071ddd105a497b616e1d436768
Jkinjt <l02jitoj@gmail.com>
2022-03-02 13:50:30 +0100

    Rutas establecidas

--- src/app/administrator/exchangeGift/exchange-gift-list/exchange-gift-list-routing.module.ts
+++ src/app/administrator/exchangeGift/exchange-gift-list/exchange-gift-list-routing.module.ts
@@ -6,8 +6,24 @@ import { ExchangeGiftListPage } from './exchange-gift-list.page';
 const routes: Routes = [
   {
     path: '',
-    component: ExchangeGiftListPage
-  }
+    component: ExchangeGiftListPage,
+    children:[
+      {
+        path: 'exchange-gif-update',
+        loadChildren: () => import('../exchange-gif-update/exchange-gif-update.module').then( m => m.ExchangeGifUpdatePageModule)
+      },
+      {
+        path: 'exchange-gif-saw',
+        loadChildren: () => import('../exchange-gif-saw/exchange-gif-saw.module').then( m => m.ExchangeGifSawPageModule)
+      },
+      {
+        path: 'exchange-gif-create',
+        loadChildren: () => import('../exchange-gif-create/exchange-gif-create.module').then( m => m.ExchangeGifCreatePageModule)
+      },
+
+    ]
+  },
+  
 ];
 
 @NgModule({
