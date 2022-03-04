081f1177dadd187fc7c37f054efec99d0c8f548e
DavidSM24 <david.dam.4.1.96@gmail.com>
2022-03-04 20:52:46 +0100

    subiendo branch

--- src/app/administrator/agency/agency-create/agency-create.module.ts
+++ src/app/administrator/agency/agency-create/agency-create.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { AgencyCreatePageRoutingModule } from './agency-create-routing.module';
 
 import { AgencyCreatePage } from './agency-create.page';
+import { InsuranceCompanyService } from '../../../services/insurance-company.service';
+import { AgencyService } from 'src/app/services/agency.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AgencyCreatePageRoutingModule
+    AgencyCreatePageRoutingModule,
+
+    FormsModule,
+    ReactiveFormsModule,
   ],
+  providers:[AgencyService,InsuranceCompanyService],
   declarations: [AgencyCreatePage]
 })
 export class AgencyCreatePageModule {}
