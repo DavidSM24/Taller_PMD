ccc001c29b5389af7e8fa768c0c222847caf4b2c
Jkinjt <l02jitoj@gmail.com>
2022-03-04 22:07:27 +0100

    pasar id de una pantalla a otra

--- src/app/administrator/carRepair/car-repair-update/car-repair-update.page.ts
+++ src/app/administrator/carRepair/car-repair-update/car-repair-update.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CarRepair } from 'src/app/models/CarRepair';
 import { CarRepairService } from 'src/app/services/car-repair.service';
 
 @Component({
@@ -10,12 +12,30 @@ import { CarRepairService } from 'src/app/services/car-repair.service';
 export class CarRepairUpdatePage implements OnInit {
 
   public formCarRepair:FormGroup;
+  public id:number;
+  private carRepair:CarRepair;
 
   constructor(
     private carRepairService:CarRepairService,
-    private formBuilder:FormBuilder
+    private formBuilder:FormBuilder,
+    private activateRoute:ActivatedRoute,
   ) {
-    
+    this.activateRoute.params.subscribe(parametros=>{
+      
+      this.id=parametros['id'];
+      console.log(this.id);
+      
+      (async ()=>{
+        try{
+          this.carRepair=await this.carRepairService.getById(this.id);
+          this.carRepair.id=this.id;
+          
+          console.log(this.carRepair);
+        }catch(err){
+          console.log(err+"Fallo al cargar el presupuesto")
+        }
+      })();
+    })
    }
 
   ngOnInit() {
