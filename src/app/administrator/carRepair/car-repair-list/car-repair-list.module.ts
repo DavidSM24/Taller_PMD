7f779295b6ea084df7c490ede691407e558ffe89
Jkinjt <l02jitoj@gmail.com>
2022-03-05 01:01:11 +0100

    cargar elementos

--- src/app/administrator/carRepair/car-repair-list/car-repair-list.module.ts
+++ src/app/administrator/carRepair/car-repair-list/car-repair-list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +15,9 @@ import { CarRepairListPage } from './car-repair-list.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CarRepairListPageRoutingModule
+    CarRepairListPageRoutingModule,
+
+    ReactiveFormsModule
   ],
   declarations: [CarRepairListPage]
 })
