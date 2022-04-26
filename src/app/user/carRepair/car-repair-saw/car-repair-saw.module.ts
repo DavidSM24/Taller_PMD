5984cffeb51a415592cdd7e1f4ccd4ba46f793d3
Jkinjt <l02jitoj@gmail.com>
2022-04-26 19:49:22 +0200

    SawCarRepair

--- src/app/user/carRepair/car-repair-saw/car-repair-saw.module.ts
+++ src/app/user/carRepair/car-repair-saw/car-repair-saw.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CarRepairSawPageRoutingModule } from './car-repair-saw-routing.module';
 
 import { CarRepairSawPage } from './car-repair-saw.page';
+import { CarRepairService } from 'src/app/services/car-repair.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CarRepairSawPageRoutingModule
+    CarRepairSawPageRoutingModule,
+
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers:[
+    CarRepairService
   ],
   declarations: [CarRepairSawPage]
 })
