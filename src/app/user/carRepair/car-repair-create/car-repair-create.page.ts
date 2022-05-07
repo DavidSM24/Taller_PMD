9bb7a67797effd0074c956d1e460dbd3bc76fb07
Jkinjt <l02jitoj@gmail.com>
2022-05-07 22:04:28 +0200

    logica creada

--- src/app/user/carRepair/car-repair-create/car-repair-create.page.ts
+++ src/app/user/carRepair/car-repair-create/car-repair-create.page.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { IonToggle, LoadingController, PickerController, ToastController } from '@ionic/angular';
+import { IonDatetime, IonToggle, LoadingController, PickerController, ToastController } from '@ionic/angular';
 import { Agency } from 'src/app/models/Agency';
 import { CarRepair } from 'src/app/models/CarRepair';
 import { AgencyService } from 'src/app/services/agency.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { CarRepairService } from 'src/app/services/car-repair.service';
+import { DateTimeServiceService } from 'src/app/services/date-time-service.service';
 import { UtilService } from 'src/app/services/util.service';
-
+import { format, parseISO } from 'date-fns';
 @Component({
   selector: 'app-car-repair-create',
   templateUrl: './car-repair-create.page.html',
@@ -16,10 +17,22 @@ import { UtilService } from 'src/app/services/util.service';
 export class CarRepairCreatePage implements OnInit {
 
   @ViewChild(IonToggle) toggle: IonToggle;
+   //variables para la fecha
+   @ViewChild(IonDatetime) datetime: IonDatetime;
+   //Guarda la fecha con el formato de españa
+   public spanishDateOrder:string;
+ 
+   //Guarda la fecha de la reparación con el formato de españa
+   public spanishDateRepair:string;
+   //Guarda la fecha de la reparación con un formato legible para el ion-dateTime
+   public stringDateOrder:string;
+   //variable que guarda los datos proporcionados por el evento del ion-dateTime
+   public dateValue = '';
+  //Variable que guarda la fecha de reparación con el formato correcto para ser enviado a la api 
+  public formatedString;
 
   public myAgency:Agency;
 
-  public carRepair:CarRepair;
   public formCarRepair:FormGroup;
   public carRepairs:CarRepair;
 
@@ -36,7 +49,8 @@ export class CarRepairCreatePage implements OnInit {
     private pickerController:PickerController,
     private utilService:UtilService,
     private authService:AuthService,
-    private agencyService:AgencyService
+    private agencyService:AgencyService,
+    private dateTimeService:DateTimeServiceService
 
   ) {
     this.createFormGroup();
@@ -49,6 +63,31 @@ export class CarRepairCreatePage implements OnInit {
   
   public async create():Promise<void>{
     this.utilService.presentLoading();
+    if(this.myAgency!=null){
+      let newCarRepair:CarRepair={
+        operation:0,
+        carPlate:this.formCarRepair.get("carPlate").value,
+        model:this.formCarRepair.get("model").value,
+        brandCar:this.formCarRepair.get("brandCar").value,
+        clienteName:this.formCarRepair.get("clienteName").value,
+        dateOrder:new Date(Date.now()),
+        nor:0,
+        amount:0,
+        dateRepair:null,
+        asigPoints:0,
+        repaired:false,
+        myAgency:this.myAgency
+      }
+
+      newCarRepair=await this.carRepairService.createOrUpdate(newCarRepair);
+      if(newCarRepair.id){
+        this.formCarRepair.reset();
+        this.utilService.presentToast("La reparación se ha guardado correctamente",'success');
+      }else{
+        this.utilService.presentToast("Ha surgido un error al intentar crear la reparacion",'danger')
+      }
+      this.utilService.hideLoading();
+    }
 
     
   }
@@ -94,5 +133,39 @@ export class CarRepairCreatePage implements OnInit {
     });
   }
 
+    //Métodos para cerrar y abrir el modal de DateTime
+   /**
+   * Metodo que se usa en el ion-DateTime para confirmar la fecha elegida
+   */
+    confirm() {
+      this.datetime.confirm(true);
+    }
+    /**
+     * Método que se usa en ion-DateTime para cerrar la ventana
+     */
+    reset() {
+      this.datetime.cancel(true);
+    }
+
+    ////Métodos para cambiar la fecha
+   /**
+   * Método que asigna la fecha de reparación cuando el usuario pulsa el botón de aceptar en el ión date time
+   * @param event 
+   */
+    changeDateRepair(event){
+      this.spanishDateOrder=this.dateTimeService.formatSpanishDateString(event);    
+      this.stringDateOrder=this.dateTimeService.formatString(event);
+      this.dateValue=event;
+      this.formatedString=this.formatDate(this.dateValue);
+  
+    }
+     /**
+   * Metodo para darle formato a la fecha y que se pueda guardar en la base de datos
+   * @param value 
+   * @returns fecha con el formato yyyy-MM-ddTHH:mm
+   */
+  formatDate(value: string):string {
+    return format(parseISO(value), 'yyyy-MM-dd HH:mm:SS');
+  }
 
 }
