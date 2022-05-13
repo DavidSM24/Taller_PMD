c6fae1a48521dda004fc18b9af5baf7001cb5041
david4.1.96@hotmail.com <david4.1.96@hotmail.com>
2022-05-13 10:53:21 +0200

    working in user_exhangeGift

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,6 +5,7 @@ import { LocalStorageService } from './local-storage.service';
 import { User } from '../models/User';
 import { UtilService } from './util.service';
 import { Agency } from '../models/Agency';
+import { AgencyService } from './agency.service';
 
 @Injectable({
   providedIn: 'root'
@@ -18,17 +19,28 @@ export class AuthService {
   constructor(
     private storage: LocalStorageService,
     private platform: Platform,
-    private router: Router) {
+    private router: Router,
+    private as:AgencyService) {
 
       this.loadSession();
     }
   public async loadSession() {
-    let user = await this.storage.getItem('tallerUser');
+    let user:any = await this.storage.getItem('tallerUser');
     console.log(user);
     if (user) {
       try{
         user = JSON.parse(user);
         this.user = user;
+
+        if(user.administrator==false){
+          console.log("entro");
+          let agency:Agency=await this.as.getByUsercode(user.code);
+          if(agency){
+            this.agency=agency;
+            console.log(this.agency);
+          }
+        }
+
       }
       catch(error){
         this.user=null;
