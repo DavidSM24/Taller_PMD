6ff590ee2022712bda72c95a892b1b86e637f32a
david4.1.96@hotmail.com <david4.1.96@hotmail.com>
2022-05-23 13:11:12 +0200

    subiendo deploy completo, fix error icon NUEVO in admin in deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,13 @@ export const environment = {
 
   herokuConfig: {
     url: "https://talleraadd.herokuapp.com"
+  },
+  firebaseConfig : {
+    apiKey: "AIzaSyASd6Lfk3484gacvHDyvbGZCWxB0M-kXgc",
+    authDomain: "tallerpmd-5810b.firebaseapp.com",
+    projectId: "tallerpmd-5810b",
+    storageBucket: "tallerpmd-5810b.appspot.com",
+    messagingSenderId: "922057635224",
+    appId: "1:922057635224:web:832850ee7482838cc167ed"
   }
 };
